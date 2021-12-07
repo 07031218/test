b9d81c7b66dc6a25645b34e008f7d53e9908931c
07031218 <minshunqiang@gmail.com>
2021-12-06 23:37:26 -0500

    2021-12-06

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ useNIC="--interface 4"
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
 apt-get install curl -y > /dev/null || yum install curl -y > /dev/null
     curl   --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" > /root/test/1.txt
-git push --mirror https://github.com/07031218/test.git
-
+git add   1.txt
+git push
