81b70cc0b0cb983c9713e5fbb556d3419cf93325
07031218 <minshunqiang@gmail.com>
2021-12-06 23:45:55 -0500

    2021-12-06

--- test.sh
+++ test.sh
@@ -3,9 +3,18 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-useNIC="--interface 4"
-UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
+#UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
 apt-get install curl -y > /dev/null || yum install curl -y > /dev/null
     curl   --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" > /root/test/1.txt
 git add   1.txt
+if [ $# -eq 0 ]
+then
+    pushmessage=`date +%Y-%m-%d`
+else
+    pushmessage="$*"
+fi
+
+
+
+git commit -m "${pushmessage}"
 git push
